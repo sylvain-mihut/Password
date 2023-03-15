00577df26ce85ae8b17339eba2c45ed4bee81e5f
sylvain-mihut <sylvain.mihut@laplateforme.io>
2023-03-15 15:11:37 +0100

    project end

--- password.py
+++ password.py
@@ -2,15 +2,21 @@ import hashlib
 import json
 
 def verif():
-    while True:    
+    while True:
+        # Définition des caractères nécessaires pour un mot de passe fort
         minuscule =  "abcdefghijklmnopqrstuvwxyz"
         majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         special_char = "(!, @, #, $, %, ^, &, *, .)"
         chiffre = "0123456789"
-        l, u, p, d = 0, 0, 0, 0,
+        # Initialisation des compteurs pour les caractères requis
+        l, u, p, d = 0, 0, 0, 0
+
+        # Demande de saisie d'un mot de passe à l'utilisateur
         password = input("Entre un mot de passe : ")
 
+        # Vérification de la longueur du mot de passe saisi
         if len(password) >= 8:
+            # Vérification de la présence des caractères requis dans le mot de passe
             for i in password:
 
                 if i in minuscule:
@@ -27,6 +33,7 @@ def verif():
 
         if len(password) < 8 :
             print("Votre mot de passe doit contenir au moins 8 caractère")
+        # Affichage d'un message d'erreur si le mot de passe ne remplit pas les critères requis
         elif l ==0:
             print("Le mot de passe dois contenir au moins une minuscule")
         elif u == 0:
@@ -36,15 +43,35 @@ def verif():
         elif d == 0:
             print("Le mot de passe dois contenir au moins une caractère spécial (!, @, #, $, %, ^, &, *, .)")
         else:
+            # Si le mot de passe est valide, retourner le mot de passe
             print("Mot de passe validé")
             return(password)
-        
 
-Password = (verif())
+# Appel de la fonction de vérification de mot de passe et 
+Password = verif()
 
-hash = hashlib.sha256(Password.encode()).hexdigest()
-# print(hash)
+def cryptage():
+    # Hashage du mot de passe avec l'algorithme SHA-256
+    hash = hashlib.sha256(Password.encode()).hexdigest()
+    # print(hash)
+    return hash
+hash = cryptage()
 
+def check():
+    with open("data.json", "r") as temp:
+        load = json.load(temp)
+        for check in load["mdp"]:
+            # for value in check['mdp']:
+            print(check['mdp'])
+            print(hash)
+            if check['mdp'] == hash:
+                print("Le mot de passe existe deja veuillez en choisir un autre")
+                verif()
+            else:
+                print("enregistrement dans la base de donné ")
+
+check()
+#Fonction pour sauvegarder le hash du mot de passe dans un fichier JSON
 def save_json(mdp):
     with open("data.json", "r+") as fichier:
         save = json.load(fichier)
@@ -52,9 +79,5 @@ def save_json(mdp):
         fichier.seek(0)
         json.dump(save, fichier, indent=4) 
 
+# Appel de la fonction pour sauvegarder le hash du mot de passe dans le fichier JSON
 save_json(hash)
-# my_dict = hash 
-# jsonData = json.dumps(my_dict)
-# jsonFile = open("data.json", "a")
-# jsonFile.write(jsonData)
-# jsonFile.close()
